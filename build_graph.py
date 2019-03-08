24e09f89984354440564bd59e195e1abb3ade9cd
Peter Xenopoulos <pxenopoulos@10-18-145-213.dynapool.wireless.nyu.edu>
2019-03-08 15:30:22 -0500

    added basic build graph functions, init communities csv

--- build_graph.py
+++ build_graph.py
@@ -20,13 +20,38 @@ def defineCommunity(data):
     '''
     return None
 
+def preprocessData(data):
+    ''' Preprocess and clean the data
+    @param data DataFrame: Data frame containing citation information
+    '''
+    data = data.apply(lambda x: x.astype(str).str.lower())
+    return data
+
 def initGraph(data):
     ''' Initialize a graph using the different paper ids
+    @param data DataFrame: Data frame containing citation information
     '''
+    G = nx.Graph()
+    for id in data['id']:
+        G.add_node(id)
+    community_dict = data[['id', 'venue']]
+    nx.set_node_attributes(G, 'community', community_dict)
+    return G
 
-def addEdgesGraph(data):
+def addEdgesGraph(graph, data):
     ''' Adds edges between different nodes
+    @param graph Graph: Graph containing nodes from initGraph()
+    @param data DataFrame: Data frame containing citation information
     '''
-
+    for index, row in data.iterrows():
+        id = row['id']
+        refs = row['references']
+        # if not there or NaN, then add no edges, else
+        for ref in refs:
+            graph.add_edge(id, ref)
+    return graph
 
 test = readJSON("data/")
+
+for index, row in test.iterrows():
+    print(row['id'])
