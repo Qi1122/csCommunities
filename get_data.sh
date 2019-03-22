623a70bae7619afbdcf34a95a8a1bec37322fd3f
pnxenopoulos <pnxenopoulos@gmail.com>
2019-03-22 14:07:01 -0400

    added install data script

---
+++ get_data.sh
@@ -0,0 +1,9 @@
+cd python
+mkdir data
+cd data
+wget https://lfs.aminer.cn/lab-datasets/citation/dblp.v10.zip || curl -O https://lfs.aminer.cn/lab-datasets/citation/dblp.v10.zip
+unzip *,zip
+rm -rf *.zip
+cd ../..
+touch .gitignore
+echo 'python/data/' > .gitignore
