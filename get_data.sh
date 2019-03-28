d2ed2f26113a81e71c1d4d2e632e3014115744a6
Peter Xenopoulos <pnxenopoulos@gmail.com>
2019-03-28 10:31:40 -0400

    fixed small errors in py files, expanded install

--- get_data.sh
+++ get_data.sh
@@ -1,10 +1,15 @@
 cd python
 mkdir data
+mkdir graphs
 cd data
 wget https://lfs.aminer.cn/lab-datasets/citation/dblp.v10.zip || curl -O https://lfs.aminer.cn/lab-datasets/citation/dblp.v10.zip
-unzip *,zip
+unzip *.zip
 rm -rf *.zip
 cd ../..
 touch .gitignore
 echo 'python/data/' > .gitignore
 echo 'python/graphs/' >> /gitignore
+cd python
+Rscript scraped_communities.R
+python fuzzy_community_match.py
+python build_community_graph.py
--- python/build_community_graph.py
+++ python/build_community_graph.py
@@ -89,3 +89,5 @@ def prepareData(path, years):
     return citation_data
 
 citation_data = prepareData('data/', ['2018', '2017', '2016', '2015', '2014', '2013', '2012', '2011', '2010', '2009', '2008', '2007', '2006', '2005', '2004', '2003', '2002', '2001', '2000', '1999', '1998'])
+citation_data.to_csv('graphs/references.csv', index = False)
+citation_data.groupby(['paper_venue', 'reference_venue', 'paper_community', 'reference_community', 'year']).size().reset_index().to_csv('graphs/grouped_data.csv', index = False)
