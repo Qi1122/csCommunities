bd5ed8f50d891bfb71654a3456515382e70e2dea
Peter Xenopoulos <pnxenopoulos@gmail.com>
2019-04-01 11:36:12 -0400

    added prints to install

--- install.sh
+++ install.sh
@@ -1,3 +1,4 @@
+echo "---------- Making necessary directories ----------\n"
 cd python
 mkdir data
 mkdir graphs
@@ -5,13 +6,16 @@ cd graphs
 mkdir graph_data
 cd ..
 cd data
+echo "---------- Downloading data ----------\n"
 wget https://lfs.aminer.cn/lab-datasets/citation/dblp.v10.zip || curl -O https://lfs.aminer.cn/lab-datasets/citation/dblp.v10.zip
 unzip *.zip
 rm -rf *.zip
 cd ../..
+echo "---------- Create .gitignore ----------\n"
 touch .gitignore
 echo 'python/data/' > .gitignore
 echo 'python/graphs/' >> /gitignore
+echo "---------- Writing data (can take a while) ----------\n"
 cd python
 Rscript scraped_communities.R
 python fuzzy_community_match.py
